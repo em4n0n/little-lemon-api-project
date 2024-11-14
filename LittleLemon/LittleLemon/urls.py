2b536752c9019d218c4941b1ab3819514040019e
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-11-14 14:24:52 -0800

    route debug toolbar url

--- LittleLemon/LittleLemon/urls.py
+++ LittleLemon/LittleLemon/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('LittleLemonAPI.urls')),
+    path('__debug__', include('debug_toolbar.urls')),
     
 ]
