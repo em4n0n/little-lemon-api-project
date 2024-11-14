bebc0dfed03f1ab12e979f72844598f85fd02ae0
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-11-13 20:37:27 -0800

    create a MenuItem model with category as the foreign key

--- LittleLemon/LittleLemonAPI/models.py
+++ LittleLemon/LittleLemonAPI/models.py
@@ -7,3 +7,9 @@ class Category(models.Model):
     slug = models.SlugField()
     title = models.CharField(max_length=255, db_index=True)
     
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    featured = models.BooleanField(db_index=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    
