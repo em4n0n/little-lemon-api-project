bea123618aa8654e6ac53cedf17e6e1b8f74abb4
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-11-14 19:47:59 -0800

    create CategoryView

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets, status
 from django.contrib.auth.models import User, Group
-from .models import Category, MenuItem, Cart, Order, OrderItems
-
+from .models import Category, MenuItem, Cart, Order, OrderItem
+from .serializers import CategorySerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 # Create your views here.
+class CategoryView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializers_class = CategorySerializer
+    
+    def get_permissions(self):
+        permisison_classes = []
+        if self.request.method != 'GET':
+            permission_classes = [IsAuthenticated]
+            
+        return [permission() for permission in permisison_classes]
+    
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]    
+    
