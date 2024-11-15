9082d0a9493602df092e1d7609d81b0df3b6771a
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-11-14 19:40:22 -0800

    import rest framework and models

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets, status
+from django.contrib.auth.models import User, Group
+from .models import Category, MenuItem, Cart, Order, OrderItems
 
 # Create your views here.
