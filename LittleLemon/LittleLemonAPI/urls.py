56be4dc5626f235ab20a8f3aa17227dc4b7eb6b3
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-11-17 15:26:22 -0800

    route url path for menu items

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('category', views.CategoryView.as_view())
+    path('category', views.CategoryView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuView.as_view()),
 ]
 
