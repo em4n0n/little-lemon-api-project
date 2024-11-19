d6f228871a17b609aedf0defa5d6adecee8894f3
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-11-18 21:14:30 -0800

    Add serializers for Category and MenuItems with data validation

--- LittleLemon/LittleLemonAPI/serializers.py
+++ LittleLemon/LittleLemonAPI/serializers.py
@@ -1,3 +1,19 @@
 from rest_framework import serializers
-from .models import Category, MenuItem, Cart, Order, OrderItem
+from .models import Category, MenuItems, Cart, Order, OrderItem
+import bleach
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'title', 'slug']
+            
+class MenuItemsSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.IntegerField(write_only=True)
+    
+    def validate_title(self, value):
+        return bleach.clean(value)
+    
+    class Meta:
+        model = MenuItems
+        fields = ['id', 'user', 'menuitems', 'quantity', 'unit_price', 'price', 'menuitem_id']
