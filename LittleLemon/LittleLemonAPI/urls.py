2b17b411c92717168f7fa3722692b258153741c2
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-11-21 17:32:34 -0800

    route manager view and permissions for manager

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -4,9 +4,10 @@ from rest_framework.authtoken.views import obtain_auth_token
 from . import permissions
 
 urlpatterns = [
-    path('api-token-auth/', obtain_auth_token)
+    path('api-token-auth/', obtain_auth_token),
     path('category', views.CategoryView.as_view()),
     path('menu-items', views.CategoryView.as_view()),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('groups/manager/users', permissions.manager_view)
 ]
 
