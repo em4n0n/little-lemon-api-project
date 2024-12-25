1f895a6339667118705fcdeb74512180dfb3c225
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-12-25 15:16:12 -0800

    route path for delivery crew

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('menu-items', views.CategoryView.as_view()),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
     path('groups/manager/users', permissions.manager_view),
+    path('groups/delivery-crew/users', views.DeliveryCrewView.as_view()),
+    path('groups/delivery=crew/users/<int:pk>', views.DeliveryCrewView.as_view()),
     path('cart/menu-items', views.CartView.as_view())
 ]
 
